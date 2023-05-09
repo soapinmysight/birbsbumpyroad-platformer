ddab148398050fdfa79c7ebaa46043dc3a85212c
soapinmysight <dieuwvr@gmail.com>
2023-05-09 14:55:44 +0200

    mainly added images

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,8 +1,10 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import fishImage from '../images/fish.png'
+import knightImage from '../images/knight.png' 
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
+    Fish: new ImageSource(fishImage),
+    Knight: new ImageSource(knightImage)
 }
 const ResourceLoader = new Loader([Resources.Fish])
 
