c6bc50852d25dc045207ca0500a83f0d84ba226f
soapinmysight <dieuwvr@gmail.com>
2023-05-16 15:57:36 +0200

    comments

--- src/js/knight.js
+++ src/js/knight.js
@@ -1,4 +1,6 @@
+// Importing specific classes from the "excalibur" module
 import{Actor, Engine, Vector, Random, CollisionType} from "excalibur"
+// Importing specific classes from the "resources.js" file
 import {Resources, ResourceLoader} from './resources.js'
 
 export class Knight extends Actor {
@@ -11,30 +13,37 @@ export class Knight extends Actor {
 }
     onInitialize(engine){
         this.body.useGravity  = true
+        // Enabling gravity for the actor's body
         this.body.collisionType = CollisionType.Active;
+        // Setting the collision type of the actor's body to "Active"
         this.graphics.add(Resources.Knight.toSprite());
+        // Adding the knight's sprite from the Resources module to the actor's graphics
         this.scale = new Vector (0.4,0.4);
+        // Setting the scale of the actor
     }
 
 OnPreUpdate(engine) {
 
+        // Variable to store the horizontal speed of the knight
         let xspeed = 0;
+        // Variable to store the vertical speed of the knight
         let yspeed = 0;
 
         if(engine.input.keyboard.isHeld(Input.Keys.D)){
-
+            // Code for handling the D key being held
         }
-    }
-
-    run(){
-
-    }
-
-    jump(){
-
-    }
-
-    attack(){
-
-    }
 }
+
+run() {
+    // Code for the knight running
+}
+
+jump() {
+    // Code for the knight jumping
+}
+
+attack() {
+    // Code for the knight attacking
+}
+}
+
