1f9a1d923a74a7adb7a2600d27caebd8571ea3ff
soapinmysight <dieuwvr@gmail.com>
2023-06-25 00:43:27 +0200

    cleanign code creating errors

--- src/js/player.js
+++ src/js/player.js
@@ -1,130 +1,105 @@
-import * as ex from "excalibur";
-import { Actor, Engine, Vector, Random, CollisionType, Input, Axis } from "excalibur"
-import { Resources, ResourceLoader } from "./resources";
+// Get all the important stuff from excalibur
+import { Vector, Input, Actor, CollisionType, Shape } from "excalibur"
+// Get all the classes which we wrote
+import { Resources } from "./resources";
 import { platform } from "./platform";
-import { Startscreen } from "./startscreen.js";
 import { Nest } from "./nest";
 import { Enemy } from "./enemynormal";
 import { Enemylegs } from "./enemylegs";
 import { Enemyknife } from "./enemyknife";
 
-// Define a class named "Mainplayer" that extends the "ex.Actor" class
-export class Mainplayer extends ex.Actor {
+// making the player class
+export class Mainplayer extends Actor {
 
     //Declare global variables
-    speed
-    // jumped = false
+    sprite;
     grounded = true
-    x
-    y
     engine
-    score
 
-    constructor(score) {
+    constructor() {
         super({
-            //set collisiontype to active
-            collisionType: ex.CollisionType.Active,
-            collider: ex.Shape.Box(250, 350, ex.Vector.Half, ex.vec(0, 0)),
-            displayMode: ex.DisplayMode.FitScreen,
+            //set collisiontype to active so that it can interact with other objects in the scene
+            collisionType: CollisionType.Active,
+            collider: Shape.Box(250, 350, new Vector(0.5,0.5), new Vector(0, 0)),
         });
-        // Set the player's sprite and initialize other properties
-        // this.graphics.use(Resources.Birb.toSprite());
-        // this.scale = new ex.Vector(0.25, 0.25)
-        // this.pos = new ex.Vector(50, 500);
-        // this.pointer.useGraphicsBounds = true;
-        // this.enableCapturePointer = true;
-        this.score = score
     }
 
     onInitialize(engine) {
-        // this.game = engine
         this.engine = engine
-        // Enabling gravity for the actor's body
+        // setting useGravity to true so that it can fall to the ground
         this.body.useGravity = true
 
-        this.on('collisionstart', (evt) => this.onCollisionStart(evt))
+        // Whenever there is a collision happening we call the this.onCollisionStart method (function)
+        this.on('collisionstart', (e) => this.onCollisionStart(e))
     
         // Set the player's sprite and initialize other properties
-        this.graphics.use(Resources.Birb.toSprite());
-        this.scale = new ex.Vector(0.25, 0.25)
-        this.pos = new ex.Vector(50, 500);
-
-        this.pointer.useGraphicsBounds = true;
-        this.enableCapturePointer = true;
+        this.sprite = Resources.Birb.toSprite()
+        this.graphics.use(this.sprite);
+        this.scale = new Vector(0.25, 0.25)
+        this.pos = new Vector(50, 500);
     }
 
+    // This method is used to update the players position based on which key you are pressing (movement code)
     onPreUpdate(engine) {
 
+        // initially setting the speed to 0, Whenever we arent pressing any keys we dont want to move :|
         let xspeed = 0
         let yspeed = 0
 
+        // Assigning the keyboard class to kb
         let kb = engine.input.keyboard
 
-        if (kb.isHeld(ex.Input.Keys.W) || kb.isHeld(Input.Keys.Up)) {
-            // Handle jump
-            //make sure you can only jump when on the ground
+        // check wether the W key is held or the Up arrow key is held
+        if (kb.isHeld(Input.Keys.W) || kb.isHeld(Input.Keys.Up)) {
+            // If we are grounded and not in the air
             if (this.grounded == true) {
-                //jump
+                // set the yspeed to -500 to jump
                 yspeed = -500
-                console.log(yspeed)
-                //set grounded to false
+                // set this.gounded to false so that we cant jump in the air
                 this.grounded = false
-                this.jumped = true
             }
-
-        
         }   
-            //        if (kb.isHeld(Input.Keys.S) || kb.isHeld(Input.Keys.Down)) {
-            //            yspeed = 300
-            //        }
-
-
-            if (kb.isHeld(Input.Keys.A) || kb.isHeld(Input.Keys.Left)) {
-                xspeed = -300
-                // optioneel, flip de sprite
-                // this.sprite.flipHorizontal = true
-            }
-            if (kb.isHeld(Input.Keys.D) || kb.isHeld(Input.Keys.Right)) {
-                xspeed = 300
-                // optioneel, flip de sprite
-                // this.sprite.flipHorizontal = false
-            }
-
-            //        // schieten en springen gebeurt maar 1 keer na een press
-            //        if (engine.input.keyboard.wasPressed(Input.Keys.Space)) {
-            //            console.log("jump!")
-            //       }
-
-            this.vel = new Vector(xspeed, this.vel.y + yspeed)
-            engine.currentScene.camera.x = this.pos.x + 200
+        // check wether the A key is held or the Left arrow key is held
+        if (kb.isHeld(Input.Keys.A) || kb.isHeld(Input.Keys.Left)) {
+            // set the xspeed to -300 to move to the left
+            xspeed = -300
+            // Setting the sprite variable to flip horizontally
+            this.sprite.flipHorizontal = true
+            // using this sprite to change the way the sprite looks
+            this.graphics.use(this.sprite)
+        }
+        // check wether the D key is held or the Right arrow key is held
+        if (kb.isHeld(Input.Keys.D) || kb.isHeld(Input.Keys.Right)) {
+            // set the xspeed to 300 to move to the right
+            xspeed = 300
+            // Setting the sprite variable to flip horizontally
+            this.sprite.flipHorizontal = false
+            // using this sprite to change the way the sprite looks
+            this.graphics.use(this.sprite)
+        }
+        
+        // After checking which buttons were pressed we update the speed of the players
+        this.vel = new Vector(xspeed, this.vel.y + yspeed)
+        // make sure the camera is following us at all times
+        engine.currentScene.camera.x = this.pos.x + 200
     }
 
 
-    // update(engine) {
-    //     if (this.vel.x > 0) {
-    //         this.vel.x -= 10;
-    //     } else if (this.vel.x < 0) {
-    //         this.vel.x += 10;
-    //     }
-
-    //     if (this.vel.y === 0) {
-    //         this.grounded = true;
-    //         this.jumped = false;
-    //     } else {
-    //         this.grounded = false;
-    //     }
-    // }
-
-
-
-    onCollisionStart(event) {
-        if (event.other instanceof Nest) {
+    // Whenever we collide with something this method (function) is called
+    onCollisionStart(e) {
+        // checks wether we collided with a nest 
+        //and if we did we would go to the Victory screen and passing the score trough.
+        if (e.other instanceof Nest) {
             this.engine.goToScene('Victoryscreen', this.engine.currentScene.score)
         }
-        if (event.other instanceof Enemy || event.other instanceof Enemylegs || event.other instanceof Enemyknife) {
+        // checks wether we collided with any of the three enemies 
+        //and if we did we would go to the Gameoverscreen and passing the score trough
+        if (e.other instanceof Enemy || e.other instanceof Enemylegs || e.other instanceof Enemyknife) {
             this.engine.goToScene('Gameoverscreen', this.engine.currentScene.score)
         }
-        if (event.other instanceof platform && this.vel.y === 0) {
+        // if we are colliding with a platform and not gliding trough the air like a majestic bird. 
+        //Put the grounded variable to true again so that we may jump once more.
+        if (e.other instanceof platform && this.vel.y === 0) {
             this.grounded = true
         }
     }
